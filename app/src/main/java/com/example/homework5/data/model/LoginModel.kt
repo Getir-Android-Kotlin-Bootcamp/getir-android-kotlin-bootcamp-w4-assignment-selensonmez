9112bc28185aa1665b863a38dc2fb43fb7684e8a
selensonmez <selensonmez2001@gmail.com>
2024-04-12 14:33:45 +0300

    models

---
+++ app/src/main/java/com/example/homework5/data/model/LoginModel.kt
@@ -0,0 +1,6 @@
+package com.example.homework5.data.model
+
+data class LoginModel(
+    val email: String,
+    val password: String,
+)
---
+++ app/src/main/java/com/example/homework5/data/model/User.kt
@@ -0,0 +1,14 @@
+package com.example.homework5.data.model
+
+data class User(
+    val id: String,
+    val fullName: String,
+    val email: String,
+    val password: String,
+    val phoneNumber: String,
+    val occupation: String,
+    val employer: String,
+    val country: String,
+    val latitude: String,
+    val longitude: String,
+)
